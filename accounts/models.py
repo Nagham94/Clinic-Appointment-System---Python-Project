b5a0d15a0f558ee00ac9e72fef599c2a412c2664
nagham94 <naghamfathy94@gmail.com>
2026-02-27 02:13:38 +0200

    connect databse & add users accounts model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+"""
+roles -> patient / doctor / receptionist / admin
+username & email -> unique
+phone -> optional (can be empty)
+date of birth -> used to calculate patient age
+"""
+class User(models.Model):
+
+    ROLE_CHOICES = (
+        ('PATIENT', 'Patient'),
+        ('DOCTOR', 'Doctor'),
+        ('RECEPTIONIST', 'Receptionist'),
+        ('ADMIN', 'Admin'),
+    )
+
+    username = models.CharField(max_length=50, unique=True)
+    password = models.CharField(max_length=128)
+    email = models.EmailField(unique=True)
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES)
+    phone = models.CharField(max_length=20, blank=True, null=True)
+    date_of_birth = models.DateField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+# return -> username + role -> used in the panel so that each user appears as username(role)
+    def __str__(self):
+        return f"{self.username} ({self.role})"
--- clinic_system/settings.py
+++ clinic_system/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
+    'scheduling',
+    'appointments',
+    'medical',
+    'dashboard',
 ]
 
 MIDDLEWARE = [
@@ -72,10 +77,21 @@ WSGI_APPLICATION = 'clinic_system.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'clinic_db',
+        'USER': 'postgres',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
@@ -104,7 +120,8 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+#TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Cairo'
 
 USE_I18N = True
 
@@ -120,3 +137,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
